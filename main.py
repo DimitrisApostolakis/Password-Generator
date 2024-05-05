cb89a320cccaa9bcfc4a3ec9e6a2d96e61bd1b8c
Dimitris Apostolakis <129792515+DimitrisApostolakis@users.noreply.github.com>
2024-05-05 20:55:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import string
+import secrets
+
+
+def passwordGenerator(given_length):
+    filtered_symbols = "!#@*&_"
+    random_output = "".join(
+        secrets.choice(string.ascii_letters + string.digits + filtered_symbols) for _ in range(given_length))
+    return random_output
+
+
+while True:
+    try:
+        length = int(input("Choose the length of the password: "))
+        if not 6 <= length <= 32:
+            print("Please provide an number between 6 - 32!")
+            continue
+        else:
+            print(passwordGenerator(length))
+            break
+    except ValueError:
+        print("Please provide an integer number!")
+        continue
